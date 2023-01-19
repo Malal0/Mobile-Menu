d55340124227f24bc2ccb54f36e62d03e4809b15
UncomfortableChai <mj1055119@gmail.com>
2023-01-18 21:26:30 -0800

    gave the remove btns functionality

--- index.js
+++ index.js
@@ -1,5 +1,5 @@
 import { foodData } from '/data.js'
-console.log(foodData); // delete soon
+
 const menuitemsContainer = document.querySelector('#menu-items');
 
 const menuOrders = [];
@@ -12,23 +12,44 @@ menuitemsContainer.innerHTML = foodData.map(food => `
 <p class="item-ingredients">${food.ingredients}</p>
 <p class="item-price">$${food.price}</p>
 </div>
-<button class="increment-btn" data-id=${food.emoji}>+</button>
+<button class="increment-btn" data-id=${food.emoji} data-action='add'>+</button>
 </div>
 `).join(' ');
 
 const ordersContainer = document.querySelector('#orders-container');
-const addFoodBtns = document.querySelectorAll('button[class="increment-btn"]');
+const orderTotalPriceEl = document.querySelector('#order-total-price');
+const orderDetails = document.querySelector('#order-details');
 
-addFoodBtns.forEach(btn => {
-    btn.addEventListener("click", addFood)
-})
+document.addEventListener('click', documentClick);
+
+function documentClick(e) {
+    console.log(e.target)
+    if (e.target.dataset.action) {
+        handleIncrementBtn(e)
+    }
+}
+
+function handleIncrementBtn(e) {
+    const action = e.target.dataset.action;
+    if (action === 'add') {
+        addFood(e)
+    } else if (action === 'remove') {
+        subtractFood(e)
+    }
+    menuOrders.length ? orderDetails.classList.remove('hidden') : orderDetails.classList.add('hidden');
+    renderOrders();
+    console.log(orderDetails.classList)
+}
 
 function addFood(e) {
-    // console.log(e.target.dataset.id)
     const item = foodData.filter(food => food.emoji === e.target.dataset.id)[0];
     menuOrders.push(item);
-    console.log(menuOrders)
-    renderOrders();
+}
+
+function subtractFood(e) {
+    const item = foodData.filter(food => food.emoji === e.target.dataset.id)[0];
+    menuOrders.splice(menuOrders.indexOf(item), 1);
+    console.log(menuOrders);
 }
 
 function renderOrders() {
@@ -36,8 +57,16 @@ function renderOrders() {
         `
         <div class="order">
             <h2 class="order-title">${item.title}</h2>
-            <button class="order-remove-btn">remove</button>
+            <button class="order-remove-btn" data-id=${item.emoji} data-action="remove">remove</button>
             <p class="order-price">$${item.price}</p>
         </div>
     `).join(' ');
+
+    orderTotalPriceEl.innerHTML = getTotalPrice();
+}
+
+function getTotalPrice() {
+    return menuOrders.reduce((total, item) =>
+        total + item.price
+        , 0);
 }
